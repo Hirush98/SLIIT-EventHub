397230ef61171fe796007322ecc9aa4e95060613
Hirush98 <161017466+Hirush98@users.noreply.github.com>
2026-03-27 20:31:01 +0530

    Create adminApi.js

---
+++ SLIIT-EventHub/client/src/api/adminApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+const adminClient = axios.create({
+  baseURL: `${BASE_URL}/admin`,
+  headers: { 'Content-Type': 'application/json' }
+});
+
+adminClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('eh_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+const adminApi = {
+  getAllUsers:       async (params = {}) => (await adminClient.get('/users', { params })).data,
+  getPlatformStats: async ()             => (await adminClient.get('/stats')).data,
+  changeUserRole:   async (id, role)     => (await adminClient.put(`/users/${id}/role`, { role })).data,
+  toggleUserStatus: async (id, isActive) => (await adminClient.put(`/users/${id}/status`, { isActive })).data,
+};
+
+export default adminApi;
