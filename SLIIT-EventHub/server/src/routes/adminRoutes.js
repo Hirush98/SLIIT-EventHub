8bf7be94a56e21eb3932ca7fab1737fbc50b1a68
Hirush98 <161017466+Hirush98@users.noreply.github.com>
2026-03-27 20:12:07 +0530

    Create admin routes

---
+++ SLIIT-EventHub/server/src/routes/adminRoutes.js
@@ -0,0 +1,20 @@
+const express  = require('express');
+const router   = express.Router();
+const { protect, restrictTo } = require('../middleware/authMiddleware');
+const {
+  getAllUsers,
+  changeUserRole,
+  toggleUserStatus,
+  getPlatformStats
+} = require('../controllers/adminController');
+
+// All admin routes require login + admin role
+router.use(protect);
+router.use(restrictTo('admin'));
+
+router.get('/users',                getAllUsers);
+router.get('/stats',                getPlatformStats);
+router.put('/users/:id/role',       changeUserRole);
+router.put('/users/:id/status',     toggleUserStatus);
+
+module.exports = router;
